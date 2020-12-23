c268b326cd41d72e2edd061f21bd73fb3e6bf8b3
廖理 <liaolidianzi@gmail.com>
2020-12-23 20:22:14 +0800

    Initial commit

---
+++ example/android/app/src/main/java/com/liaoli/plugin_demo_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.liaoli.plugin_demo_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/PluginDemoPlugin.m
@@ -0,0 +1,20 @@
+#import "PluginDemoPlugin.h"
+
+@implementation PluginDemoPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"plugin_demo"
+            binaryMessenger:[registrar messenger]];
+  PluginDemoPlugin* instance = [[PluginDemoPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
